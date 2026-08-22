400e8270de1fda38f6a6dfd8217b469fcc977c4a
netcrawlerr <abdulmejidamk@gmail.com>
2026-08-23 01:30:35 +0300

    feat(platform/linux/x11): prep pointer movement helper to X11Window

--- src/CapScroll.Platform/Linux/Native/X11/X11Window.cs
+++ src/CapScroll.Platform/Linux/Native/X11/X11Window.cs
@@ -1,6 +1,20 @@
 namespace CapScroll.Platform.Linux.Native.X11;
 
-public class X11Window
+internal static class X11Window
 {
-    
+    public static void MovePointerToRegionCenter(
+        IntPtr display,
+        Avalonia.PixelRect region)
+    {
+        var centerX =
+            region.X + region.Width / 2;
+
+        var centerY =
+            region.Y + region.Height / 2;
+
+        X11Input.MovePointer(  // will add mv ptr 
+            display,
+            centerX,
+            centerY);
+    }
 }
