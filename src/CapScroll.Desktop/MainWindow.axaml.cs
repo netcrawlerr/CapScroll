c9b936297fc718d34cdc6633d64747edb05a8bc9
netcrawlerr <abdulmejidamk@gmail.com>
2026-08-22 22:36:32 +0300

    feat(ui): initial ui that i can capitalize on

--- src/CapScroll.Desktop/MainWindow.axaml.cs
+++ src/CapScroll.Desktop/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace CapScroll.Desktop;
 
@@ -7,5 +8,15 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void CaptureButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void ScrollCaptureButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
     }
 }
