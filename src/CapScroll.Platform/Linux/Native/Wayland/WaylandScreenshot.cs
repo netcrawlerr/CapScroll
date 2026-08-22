106746c8772c89ae2e44f979ab2d097066a8638c
netcrawlerr <abdulmejidamk@gmail.com>
2026-08-22 23:07:48 +0300

    chore(platform): created the needed files for x11 impl

---
+++ src/CapScroll.Platform/Linux/Native/Wayland/WaylandScreenshot.cs
@@ -0,0 +1,6 @@
+namespace CapScroll.Platform.Linux.Native.Wayland;
+
+public class WaylandScreenshot
+{
+    
+}
---
+++ src/CapScroll.Platform/Linux/Native/X11/X11Screenshot.cs
@@ -0,0 +1,6 @@
+namespace CapScroll.Platform.Linux.Native.X11;
+
+public class X11Screenshot
+{
+    
+}
