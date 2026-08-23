bf3dfeb1e164274fb41f2b3846deef56e23c3b94
netcrawlerr <abdulmejidamk@gmail.com>
2026-08-23 19:01:57 +0300

    feat(ui): added themes controls and styles

---
+++ src/CapScroll.Desktop/Controls/ScButton.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace CapScroll.Desktop.Controls;
+
+public partial class ScButton : Button
+{
+    public ScButton()
+    {
+        InitializeComponent();
+    }
+}
