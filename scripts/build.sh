00ce7509b085f666455b930f8ce027bf7d936a6d
netcrawlerr <abdulmejidamk@gmail.com>
2026-08-26 20:40:05 +0300

    feat(desktop): add circular application logo and update package icon support

--- scripts/build.sh
+++ scripts/build.sh
@@ -65,6 +65,7 @@ mkdir -p "$DEBIAN_DIR/DEBIAN"
 mkdir -p "$DEBIAN_DIR/usr/bin"
 mkdir -p "$DEBIAN_DIR/usr/share/$PACKAGE_NAME"
 mkdir -p "$DEBIAN_DIR/usr/share/applications"
+mkdir -p "$DEBIAN_DIR/usr/share/icons/hicolor/256x256/apps"
 
 
 echo
@@ -72,6 +73,9 @@ echo "[5/7] Copying application files..."
 
 cp -r "$PUBLISH_DIR/"* \
     "$DEBIAN_DIR/usr/share/$PACKAGE_NAME/"
+
+cp "$PROJECT_DIR/src/CapScroll.Desktop/Assets/Images/logo.png" \
+    "$DEBIAN_DIR/usr/share/icons/hicolor/256x256/apps/$PACKAGE_NAME.png"
 
 
 cat << EOF > "$DEBIAN_DIR/usr/bin/$PACKAGE_NAME"
@@ -104,6 +108,7 @@ Type=Application
 Name=CapScroll
 Comment=Scrolling screenshot and region capture tool
 Exec=/usr/bin/$PACKAGE_NAME
+Icon=$PACKAGE_NAME
 Terminal=false
 Categories=Graphics;Utility;
 EOF
